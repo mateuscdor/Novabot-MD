0f2ed538d285c488c45503bca035c66b2edbf3dc
elrebelde21 <elrebelde21@gmail.com>
2022-08-07 19:35:02 -0300

    iniciado...

---
+++ config.js
@@ -0,0 +1,138 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Web\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'tu apikey',
+}
+
+global.xteam = 'RiyGanz'
+
+
+global.autoTyping = false 
+global.autoreadpmngc = false 
+global.autoReadGc = false 
+global.autoRecord = false
+global.available = true 
+
+
+global.vidmenu = fs.readFileSync("./media/theme/Cheems-bot.mp4") //gif and video menu
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.vcardowner = ['56977774748'] 
+global.ownername = "░▒▓█►─═ 𝓝𝓸𝓿𝓪𝓫𝓸𝓽-ℳ𝓓 ═─◄█▓▒░"
+global.ytname = "YT: a" 
+global.socialm = "GitHub: Novabot-MD" 
+global.location = "America, argentina" 
+//__________________
+global.vcardowner1 = ['51935116539'] 
+global.ownername1 = "░▒▓█►─═ Misael ═─◄█▓▒░"
+global.ytname1 = "YT: a" 
+global.socialm1 = "GitHub: Novabot-MD" 
+global.location1 = "America, argentina" 
+//__________________
+global.vcardowner2 = ['5218992059110'] 
+global.ownername2 = "░▒▓█►─═ tio carlos ═─◄█▓▒░"
+global.ytname2 = "YT: a" 
+global.socialm2 = "GitHub: Novabot-MD" 
+global.location2 = "America, argentina" 
+
+//bot bomdy 
+global.owner = ['56977774748' , '51935116539']
+global.ownertag = '56977774748' 
+global.botname = '𝓝𝓸𝓿𝓪𝓫𝓸𝓽-ℳ𝓓' 
+global.linkz = "https://youtube.com" 
+global.websitex = "https://youtube.com/" 
+global.botscript = 'https://github.com/Confusion' 
+global.reactmoji = "🤖" 
+global.themeemoji = "✅" 
+global.packname = " ‎𝘚𝘛𝘐𝘊𝘒𝘌𝘙  𝘉𝘠" 
+global.author = " ‎𝓝𝓸𝓿𝓪𝓫𝓸𝓽-ℳ𝓓"
+
+
+//Bot theme media
+global.thum = fs.readFileSync("./media/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./media/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./media/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./media/theme/cheemspic.jpg") //ur thumb pic
+
+
+global.premium = ['56999448163'] 
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.','#','/']
+global.sp = '⭔'
+global.mess = {
+    success: 'Listo✔️',
+    admin: '* [ ⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚂𝙾𝙻𝙾 𝙿𝚄𝙴𝙳𝙴 𝚂𝙴𝚁 𝚄𝚃𝙸𝙻𝙸𝚉𝙰𝙳𝙾 𝙿𝙾𝚁 𝙰𝙳𝙼𝙸𝙽𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾!*',
+    botAdmin: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] 𝙿𝙰𝚁𝙰 𝙿𝙾𝙳𝙴𝚁 𝚄𝚂𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙴𝚂 𝙽𝙴𝙲𝙴𝚂𝙰𝚁𝙸𝙾 𝚀𝚄𝙴 𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴𝙰 𝙰𝙳𝙼𝙸𝙽, 𝙰𝙲𝙴𝙽𝙳𝙴𝚁 𝙰 𝙰𝙳𝙼𝙸𝙽 𝙴𝚂𝚃𝙴 𝙽𝚄𝙼𝙴𝚁𝙾*',
+    owner: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚂𝙾𝙻𝙾 𝙿𝚄𝙴𝙳𝙴 𝚂𝙴𝚁 𝚄𝚃𝙸𝙻𝙸𝚉𝙰𝙳𝙾 𝙿𝙾𝚁 𝙴𝙻/𝙻𝙰 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾/𝙰 (𝙾𝚆𝙽𝙴𝚁) 𝙳𝙴𝙻 𝙱𝙾𝚃*',
+    group: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚂𝙾𝙻𝙾 𝙿𝚄𝙴𝙳𝙴 𝚂𝙴𝚁 𝚄𝚃𝙸𝙻𝙸𝚉𝙰𝙳𝙾 𝙴𝙽 𝙶𝚁𝚄𝙿𝙾𝚂*',
+    private: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚂𝙾𝙻𝙾 𝙿𝚄𝙴𝙳𝙴 𝚂𝙴𝚁 𝚄𝚃𝙸𝙻𝙸𝚉𝙰𝙳𝙾 𝙴𝙽 𝙲𝙷𝙰𝚃 𝙿𝚁𝙸𝚅𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*',
+    bot: '*⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] Esta función es solo para el bot*',
+    wait: '*[⏳] 𝙴𝚗 𝚙𝚛𝚘𝚌𝚎𝚜𝚘...*',
+    linkm: '*[❗] Donde esta el link?*',
+    error: '*[⚙️] Error*',
+    endLimit: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] Su límite diario ha expirado, el límite se restablecerá cada 12 horas*',
+    ban: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] Has sido baneado por el dueño, si quieres ser desbaneado, chatea con mi dueño*',
+    nsfw: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ]  La función nsfw no se ha activado, comuníquese con el administrador para activarla*',
+    banChat: '*[⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] El bot fue baneado en este grupo, comuníquese con el propietario para desbanear*'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 500,
+    monayawal: 200
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./media/theme/cheemspic.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Se actualizó ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
